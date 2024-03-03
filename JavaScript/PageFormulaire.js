602092e648af2591dc1fa3a3e471efb47d907bb1
Andy <Andylamothe22@hotmail.com>
2024-03-02 21:42:00 -0500

    update final

--- JavaScript/PageFormulaire.js
+++ JavaScript/PageFormulaire.js
@@ -1,35 +1,48 @@
 ////////////////// Obtenez le modal d'inscription ///////////////////////
-function userInfo(){
-  // Todo
-  const user = document.getElementsById("userI").value;
-  const pwd = document.getElementsById("pwdI").value;
+
+/**Création d'une fonction pour enregistrer les informations
+ * lors de l'inscriptions qui se mettent dans le local storage
+ */
+
+function userInfo() {
+  const user = document.getElementById("userI").value;
+  const pwd = document.getElementById("pwdI").value;
 
   localStorage.setItem("user", user);
-
   localStorage.setItem("pwd", pwd);
 }
-function connexion(){
-    // document.location='PageTache.html';
+
+/**Creation d'une fonction connexion qui va valider l'information
+ * avec les donnés enregistrer dans le local storage de la fonction (userinfo)
+ * pour une raison inconnu la validation ne marche pas
+ */
+
+function connexion() {
+  const storedUser = localStorage.getItem("user");
+  const storedPwd = localStorage.getItem("pwd");
+
+  const ConnexionUser = document.getElementById("userI").value;
+  const ConnexionPwd = document.getElementById("pwdI").value;
+
+  // Vérifiez si les informations d'identification entrées correspondent à celles stockées dans le localStorage
+  if (ConnexionUser === storedUser && ConnexionPwd === storedPwd) {
+    // Redirigez l'utilisateur vers la page souhaitée
+    window.location.href = "./PageTache.html";
+  } else {
+    alert("Identifiants incorrects. Veuillez réessayer.");
+  }
 }
 
+// Obtenez le modal d'inscription
+var modal = document.getElementById("id01");
+// Obtenez le modal de connexion
+var modal2 = document.getElementById("id02");
 
-// ////////////////// Obtenez le modal d'inscription ///////////////////////
- var modal = document.getElementById('id01');
-
-// // Lorsque l'utilisateur clique n'importe où en dehors du modal, fermez-le
- window.onclick = function(event) {
+// Lorsque l'utilisateur clique n'importe où en dehors du modal
+window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
-   }
-}
-
-
-// ///////////////// Obtenez le modal de connexion //////////////////////////
- var modal2 = document.getElementById('id02');
-
-// // Lorsque l'utilisateur clique n'importe où en dehors du modal, fermez-le
- window.onclick = function(event) {
-     if (event.target == modal2) {
-        modal.style.display = "none";
-     }
- }
+  } else if (event.target == modal2) {
+    modal2.style.display = "none";
+  }
+};
--- JavaScript/PageTache.js
+++ JavaScript/PageTache.js
@@ -1,52 +1,39 @@
-function save(){
-    let nom = document.getElementById("lastname").value;
-    let prenom = document.getElementById("firstname").value;
-
-    
-    alert(nom + ", " + prenom)
-    localStorage.setItem("nom", nom);
-    localStorage.setItem("prénom", prenom);
-
-    window.location.replace(acceuil.html);
-}
-
-
-
-
-const taskInput = document.getElementById("taskInput") ;
-
-const addTaskBtn = document.getElementById("addTask") ;
-
-const taskList = document.getElementById("taskList") ;
-
-const clearAll = document.getElementById("clearAll")
-var tasklocal = "task"
+/**les valeurs rechercher dans le formulaire html qui demande une tache
+ * taskInput qui est la tache rentrer par l'utilisateur
+ * addTaskBtn qui ajoute la tache sur le html
+ * tasklist qui met la tache dans une list
+ * tasklocal pour mettre la tache dans du local storage
+ */
+const taskInput = document.getElementById("taskInput");
+const addTaskBtn = document.getElementById("addTask");
+const taskList = document.getElementById("taskList");
+const clearAll = document.getElementById("clearAll");
+var tasklocal = "task";
 var task = "undone task ";
-var done = "task done ";
 var i = 0;
-var j = 0;
-// La fonction trim() en JavaScript est utilisée pour supprimer les espaces (espaces, tabulations et
-
-//nouvelles lignes) au début et à la fin d'une chaîne.
-
 
 addTaskBtn.addEventListener("click", addTask);
-
-
+/**fonction qui permet d'ouvrir la barre latéral */
 function w3_open() {
-    document.getElementById("mySidebar").style.display = "block";
+  document.getElementById("mySidebar").style.display = "block";
+  document.getElementById("button =").style.display = "none";
 }
- 
- function w3_close() {
-    document.getElementById("mySidebar").style.display = "none";
+/**fonction qui permet de fermer la barre latéral */
+function w3_close() {
+  document.getElementById("mySidebar").style.display = "none";
+  document.getElementById("button =").style.display = "block";
 }
-
+/** fonction qui permet
+ * d'ajouter une tache
+ * et la mettre dans une liste
+ * de la modifier
+ * de la compléter
+ * de la supprimer
+ */
 function addTask() {
+  const taskText = taskInput.value.trim();
 
-    const taskText = taskInput.value.trim();
-
-    if (taskText !==""){
-
+  if (taskText !== "") {
     const listItem = document.createElement("li");
 
     listItem.textContent = taskText;
@@ -58,55 +45,48 @@ function addTask() {
     const doneBtn = document.createElement("img");
 
     doneBtn.setAttribute("src", "./Images/done.png");
-    doneBtn.setAttribute('height', '30px');
-    doneBtn.setAttribute('width', '30px');
+    doneBtn.setAttribute("height", "30px");
+    doneBtn.setAttribute("width", "30px");
 
     listItem.appendChild(doneBtn);
 
     doneBtn.addEventListener("click", () => {
-        listItem.id = "taskDone"});
+      listItem.id = "taskDone";
+    });
 
     const editBtn = document.createElement("img");
 
     editBtn.setAttribute("src", "./Images/icons8-edit-file-50.png");
-    editBtn.setAttribute('height', '18px');
-    editBtn.setAttribute('width', '18px');
+    editBtn.setAttribute("height", "18px");
+    editBtn.setAttribute("width", "18px");
 
     listItem.appendChild(editBtn);
 
     editBtn.addEventListener("click", () => {
-        taskInput.value = listItem.textContent
-        listItem.remove()
+      taskInput.value = listItem.textContent;
+      listItem.remove();
     });
 
     const deleteBtn = document.createElement("img");
 
     deleteBtn.setAttribute("src", "./Images/delete.png");
-    deleteBtn.setAttribute('height', '30px');
-    deleteBtn.setAttribute('width', '30px');
-    deleteBtn.setAttribute('color','#ff7777');
-    deleteBtn.setAttribute('background-color','#ff7777');
+    deleteBtn.setAttribute("height", "30px");
+    deleteBtn.setAttribute("width", "30px");
 
     // La méthode JavaScript appendChild() est utilisée pour insérer un nouveau noeud ou repositionner un
-
     //noeud existant en tant que dernier enfant d'un noeud parent particulier.
-
     listItem.appendChild(deleteBtn);
 
     deleteBtn.addEventListener("click", () => {
-
-    listItem.remove();
-
+      listItem.remove();
     });
-
-
-    }else{
-        alert("Veuillez entrer une tâche valide.");
-    }
-localStorage.setItem(task.concat(i), (taskText), i++)
-clearAll.addEventListener("click", () => {
-    localStorage.clear()
-})
-localStorage.setItem(tasklocal.concat(i), taskText);
-i++;
+  } else {
+    alert("Veuillez entrer une tâche valide.");
+  }
+  localStorage.setItem(task.concat(i), taskText, i++);
+  clearAll.addEventListener("click", () => {
+    localStorage.clear();
+  });
+  localStorage.setItem(tasklocal.concat(i), taskText);
+  i++;
 }
